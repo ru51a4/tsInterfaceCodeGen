c12fd4e2b0b2a4417cd92c7867eb8b114086e40a
ru51a4 <rruslanvaleev@yandex.ru>
2024-01-06 14:12:27 +0500

    init

--- index.js
+++ index.js
@@ -11,34 +11,6 @@ class node {
 
 class tsInterfaceCodeGen {
     static gen(str) {
-        str = `{"menu": {
-    "header": "SVG Viewer",
-    "items": [
-        {"id": "Open"},
-        {"id": "OpenNew", "label": "Open New"},
-        null,
-        {"id": "ZoomIn", "label": "Zoom In"},
-        {"id": "ZoomOut", "label": "Zoom Out"},
-        {"id": "OriginalView", "label": "Original View"},
-        null,
-        {"id": "Quality"},
-        {"id": "Pause"},
-        {"id": "Mute"},
-        null,
-        {"id": "Find", "label": "Find..."},
-        {"id": "FindAgain", "label": "Find Again"},
-        {"id": "Copy"},
-        {"id": "CopyAgain", "label": "Copy Again"},
-        {"id": "CopySVG", "label": "Copy SVG"},
-        {"id": "ViewSVG", "label": "View SVG"},
-        {"id": "ViewSource", "label": "View Source"},
-        {"id": "SaveAs", "label": "Save As"},
-        null,
-        {"id": "Help"},
-        {"id": "About", "label": "About Adobe CVG Viewer..."}
-    ]
-}}
-`
         let obj = JSON.parse(str)
         let i = 0;
         let uuidv4 = () => {
@@ -49,7 +21,6 @@ class tsInterfaceCodeGen {
         start.key = "root";
         let stack = [start];
         let nodes = [];
-        let map = [];
         let createTree = (obj, prevKey, prevIsArray = false) => {
             for (let key in obj) {
                 let el = new node();
@@ -61,7 +32,9 @@ class tsInterfaceCodeGen {
                     el.value = obj[key];
                 } else {
                     stack.push(el);
-                    nodes.push(el);
+                    if (!prevIsArray) {
+                        nodes.push(el);
+                    }
                     createTree(obj[key], key, el.isArray);
                     stack.pop();
                 }
@@ -69,15 +42,16 @@ class tsInterfaceCodeGen {
         }
         createTree(obj);
         let r = [];
-        nodes = nodes.sort((a, b) => a.childrens.filter((c) => c.childrens.length).length - b.childrens.filter((c) => c.childrens.length).length)
+        nodes = nodes.sort((a, b) => a.childrens.filter((c) => c.childrens.length).length - b.childrens.filter((c) => c.childrens.length).length).filter((c) => !c.isArray);
         let resStr = '';
         let dfs = (node) => {
             resStr += `interface I${node.key} {\n`
             node?.childrens.forEach((node) => {
-
-                let arr = (node.childrens) ? '[]' : ''
+                let arr = (node.isArray && node.childrens) ? '[]' : ''
                 if (node.value === '') {
-                    resStr += `${node.key}: ${arr}I${node.key};\n`
+                    let a = nodes?.childrens?.[0].key
+                    let keyZaeb = nodes?.childrens?.filter((c) => c.key !== a)?.length;
+                    resStr += `${node.key}: ${arr}${(!keyZaeb && a) ? `I` + node.key : 'any'};\n`
                 } else {
                     resStr += `${node.key}: ${arr}${typeof node.value};\n`
                 }
@@ -95,5 +69,4 @@ class tsInterfaceCodeGen {
     }
 
 }
-console.log(tsInterfaceCodeGen.gen());
-//module.exports = tsInterfaceCodeGen
+module.exports = tsInterfaceCodeGen
